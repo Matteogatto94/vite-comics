6254b07b0ca83c411194c84f1b68c601a0722b28
Matteogatto94 <cosenza94@live.it>
2022-11-10 22:58:44 +0100

    add footer_down upgrade

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -11,36 +11,78 @@ export default {
         <div class="container">
             <div class="footer_up">
                 <div class="row">
-                    <div class="col"></div>
                     <div class="col">
-                        <img src="../assets/img/dc-logo-bg.png" alt="">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                        <ul>
+                            <h3>SHOP</h3>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+                    <div class="col">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choiches</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Rating</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+
+                    <div class="col">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <img class="logo_footer_up" src="../assets/img/dc-logo-bg.png" alt="">
                     </div>
                 </div>
             </div>
-
         </div>
-
     </footer>
 
-    <div class="footer_down">
+    <section class="contacts">
         <div class="container">
             <div class="row">
                 <div class="col">
                     <button type="button" class="btn btn-outline-light">SIGN-UP NOW!</button>
                 </div>
                 <div class="col">
-                    <span class="px-3">FOLLOW US</span>
-                    <img src="../assets/img/footer-facebook.png" alt="">
-                    <img src="../assets/img/footer-twitter.png" alt="">
-                    <img src="../assets/img/footer-youtube.png" alt="">
-                    <img src="../assets/img/footer-pinterest.png" alt="">
-                    <img src="../assets/img/footer-periscope.png" alt="">
-
+                    <span class="px-3 follow_us">FOLLOW US</span>
+                    <img class="px-3" src="../assets/img/footer-facebook.png" alt="">
+                    <img class="px-3" src="../assets/img/footer-twitter.png" alt="">
+                    <img class="px-3" src="../assets/img/footer-youtube.png" alt="">
+                    <img class="px-3" src="../assets/img/footer-pinterest.png" alt="">
+                    <img class="px-3" src="../assets/img/footer-periscope.png" alt="">
                 </div>
             </div>
         </div>
 
-    </div>
+    </section>
 
 </template>
 
