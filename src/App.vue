4852aea89d3569f6c64426efd441aee67bfd65f4
Matteogatto94 <cosenza94@live.it>
2022-11-10 15:13:39 +0100

    add color in variables

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@
 
 <template>
 
+    <header id="site_header">
+
+    </header>
+    <!-- /#site_header -->
+
+    <main id="site_main">
+
+    </main>
+    <!-- /#site_main -->
+
+    <footer id="site_footer">
+
+    </footer>
+    <!-- /#site_footer -->
+
 </template>
 
 <style>
