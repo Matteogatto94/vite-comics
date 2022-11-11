89f4799c1bfbfec313c09dc7faa6a0c79ad26cbd
Matteogatto94 <cosenza94@live.it>
2022-11-11 15:16:31 +0100

    Ho creato un components dove ho ciclato

---
+++ src/components/ComicsList.vue
@@ -0,0 +1,32 @@
+<script>
+import Comics from '../data/comics.js';
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+            comic: Comics
+        }
+    }
+}
+
+</script>
+
+
+<template>
+
+    <div class="container">
+        <div class="row">
+            <div class="col" v-for="comics in comic">
+                <img :src="comics.thumb" alt="Action Comics">
+                <p>{{ comics.series }}</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss">
+
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,21 +1,18 @@
 <script>
+import ComicsList from './ComicsList.vue';
 export default {
-    name: 'SiteMain'
+    name: 'SiteMain',
+    components: {
+        ComicsList
+    }
 }
 </script>
 
 <template>
 
     <main id="site_main">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-                        alt="Action Comics">
-                    <p>ACTION COMICS</p>
-                </div>
-            </div>
-        </div>
+
+        <ComicsList></ComicsList>
 
     </main>
 
